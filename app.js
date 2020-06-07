9d44811027b832157e0dc4d526ec3a3cd0e5127b
user_komeda <komeda@j-id.co.jp>
2020-06-07 17:02:55 +0900

    #3 signin画面を作る
    signin画面の作成
    signin画面用のroutingの作成

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
-const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const signinRouter = require('./routes/signin')
 const app = express()
 
 // view engine setup
@@ -17,8 +16,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/signin', signinRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
