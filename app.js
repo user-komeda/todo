e3206adf4681774e01f9115ca5822d96af8aafb1
user_komeda <komeda@j-id.co.jp>
2020-06-13 20:13:08 +0900

    パスワード再設定画面の作成 パスワード再設定画面のroutingの作成

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan')
 const sassMiddleware = require('node-sass-middleware')
 const signupRouter = require('./routes/signup')
 const loginRouter = require('./routes/login')
+const forgatPassRouter = require('./routes/forgat_pass.js')
 const app = express()
 
 // view engine setup
@@ -28,6 +29,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/signup', signupRouter)
 app.use('/login', loginRouter)
+app.use('/forgat-pass', forgatPassRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
