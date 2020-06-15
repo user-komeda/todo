0fda3cc2c35fdf91f2b727589d6c1381337fbcb1
user_komeda <komeda@j-id.co.jp>
2020-06-15 17:35:10 +0900

    #11 タスク一覧ページを作る
    パスワード再設定画面の作成
    パスワード再設定画面のroutingの作成

--- app.js
+++ app.js
@@ -3,26 +3,16 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-const sassMiddleware = require('node-sass-middleware')
 const signupRouter = require('./routes/signup')
 const loginRouter = require('./routes/login')
 const forgatPassRouter = require('./routes/forgat_pass.js')
 const resetPassRouter = require('./routes/reset_pass')
+const tasksRouter = require('./routes/tasks')
 const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname, 'scss/page'),
-    dest: path.join(__dirname, 'public/stylesheets/dist/'),
-    indentedSyntax: false, // true = .sass and false = .scss
-    sourceMap: false,
-    debug: true,
-    force: true,
-  })
-)
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -32,6 +22,7 @@ app.use('/signup', signupRouter)
 app.use('/login', loginRouter)
 app.use('/forgat-pass', forgatPassRouter)
 app.use('/reset-pass', resetPassRouter)
+app.use('/folder/:id/tasks', tasksRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
