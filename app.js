bb911f5e8c2e2711966a54457db8a126693974c8
user_komeda <komeda@j-id.co.jp>
2020-06-21 17:48:09 +0900

    #23 ログイン機能を作る
    passportの導入
    express-sessionの導入
    auth.jsを作る
    認証機能の作成

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('./auth')
 const signupRouter = require('./routes/signup')
 const loginRouter = require('./routes/login')
 const forgatPassRouter = require('./routes/forgat_pass.js')
@@ -20,9 +22,23 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '/public')))
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxage: 1000 * 60 * 30,
+    },
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 app.use('/signup', signupRouter)
 app.use('/login', loginRouter)
 app.use('/forgat-pass', forgatPassRouter)
